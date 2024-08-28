84f8ebf9a98484329def38122f7a616269a565c7
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-08-28 14:35:14 +0300

    add di module

--- app/src/main/java/com/noxis/notecomposeapp/activity/MainActivity.kt
+++ app/src/main/java/com/noxis/notecomposeapp/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.noxis.notecomposeapp
+package com.noxis.notecomposeapp.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.noxis.notecomposeapp.ui.theme.NoteComposeAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
